1e082f5655b0f745014648a355263028973ae70a
Carolina11 <kalei11g@gmail.com>
2017-09-12 16:29:46 -0400

    finished signIn/signUp forms

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', 'RqadPq7xp2pMLNPKgQVcglwR8a1Z11jw'),
 
     /*
     |--------------------------------------------------------------------------
